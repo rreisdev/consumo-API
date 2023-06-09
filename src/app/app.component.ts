d62f97f7ce386b9f09e053db0a2dc7c7f5743f53
rreisdev <rreis.dev@hotmail.com>
2023-06-09 11:34:46 -0300

    commit API

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { LoginService } from './login.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent  implements OnInit, OnDestroy{
+  title = 'game-mania';
+  mostraMenu:boolean =true;
+
+  constructor(private _loginService: LoginService){}
+
+  ngOnInit(): void {
+    this._loginService.getMostraMenu().subscribe(
+      res => {this.mostraMenu = res; } 
+    )
+  }
+
+  ngOnDestroy(){
+    localStorage.clear();
+      }
+}
---
+++ src/app/restrito/menu-produto/menu-produto.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/login.service';
+
+@Component({
+  selector: 'app-menu-produto',
+  templateUrl: './menu-produto.component.html',
+  styleUrls: ['./menu-produto.component.css']
+})
+export class MenuProdutoComponent {
+
+  constructor(private _router: Router,private _loginService:LoginService){}
+
+  logout(){
+    localStorage.clear();
+    this._loginService.setmostraMenu(true);
+    this._router.navigate(['/login']);
+  }
+
+}
